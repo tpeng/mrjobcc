1474d287d13dc0f0b5a6a00e74ef4d26f9bc77ba
Terry Peng <pengtaoo@gmail.com>
2016-10-21 13:03:29 +0200

    add runner

--- cc.py
+++ cc.py
@@ -9,21 +9,14 @@ from mrjob.protocol import ReprProtocol
 from mrjob.step import MRStep
 
 
-class ImportJob(MRJob):
-
-    INTERNAL_PROTOCOL = ReprProtocol
-    OUTPUT_PROTOCOL = ReprProtocol
-
-    def mapper(self, _, line):
-        v1, v2 = line.split(',')
-        yield v1, v2
-
-
 class CCJob(MRJob):
 
-    INPUT_PROTOCOL = ReprProtocol
     INTERNAL_PROTOCOL = ReprProtocol
     OUTPUT_PROTOCOL = ReprProtocol
+
+    def mapper_import(self, _, line):
+        v1, v2 = line.split(',')
+        yield v1, v2
 
     def mapper_small_star(self, v1, v2):
         if v2 <= v1:
@@ -53,57 +46,16 @@ class CCJob(MRJob):
             if v > k:
                 yield v, minv
 
-    def steps(self):
-        return [
-            MRStep(mapper=self.mapper_small_star, reducer=self.reducer_small_star),
-            MRStep(mapper=self.mapper_large_star, reducer=self.reducer_large_star)
-        ]
-
-
-class CollectJob(MRJob):
-
-    INPUT_PROTOCOL = ReprProtocol
-    INTERNAL_PROTOCOL = ReprProtocol
-    OUTPUT_PROTOCOL = ReprProtocol
-
-    def mapper(self, k, v):
+    def mapper_export(self, k, v):
         yield k, v
 
-if __name__ == "__main__":
-    input_data = [
-        [1,8],
-        [8,9],
-        [8,5],
-        [8,7],
-        [7,3],
-        [3,2],
-        [3,6],
-        [6,4]
-    ]
-    with open('test.dot', 'w') as f:
-        for v1, v2 in input_data:
-            print >> f, '%s,%s' % (v1, v2)
+    def steps(self):
+        _steps = [MRStep(mapper=self.mapper_import)]
+        for i in range(10):
+            _steps.append(MRStep(mapper=self.mapper_small_star, reducer=self.reducer_small_star))
+            _steps.append(MRStep(mapper=self.mapper_large_star, reducer=self.reducer_large_star))
+        _steps.append(MRStep(mapper=self.mapper_export))
+        return _steps
 
-    j1 = ImportJob(args=['test.dot', '--output', 'import-cc'])
-
-    max_iterations = 10
-    components = {}
-
-    with j1.make_runner() as runner1:
-        runner1.run()
-        j1_input = runner1.get_output_dir()
-
-        for i in range(max_iterations):
-            j2 = CCJob([j1_input, '--output', 'star-cc'])
-            with j2.make_runner() as runner2:
-                runner2.run()
-                j1_input = runner2.get_output_dir()
-
-        j3 = CollectJob([j1_input, '--output', 'collect-cc'])
-        with j3.make_runner() as runner3:
-            runner3.run()
-            for line in runner3.stream_output():
-                k, v = j3.parse_output_line(line)
-                components.setdefault(v, [v]).append(k)
-
-    print components
+if __name__ == '__main__':
+    CCJob().run()
---
+++ runner.py
@@ -0,0 +1,14 @@
+from cc import CCJob
+
+if __name__ == "__main__":
+    components = {}
+
+    job = CCJob()
+
+    with job.make_runner() as runner:
+        runner.run()
+        for line in runner.stream_output():
+            k, v = job.parse_output_line(line)
+            components.setdefault(v, [v]).append(k)
+
+    print components
